e9dc209e7a66d6197c686f57cec41f89113cfdd5
RenatoHK <48373263+RenatoHK@users.noreply.github.com>
2019-08-15 18:34:47 -0300

    Delete Números crescentes.py

--- Números crescentes.py
+++
@@ -1,9 +0,0 @@
-lista = input().split() #Entrada de valores na mesma linha separado por espaço
-print()
-lista = sorted(lista, key=int)
-leng = len(lista)
-i = -1
-for cont in lista:
-    if cont != leng:
-        i = i +1
-        print('Número %d:' % (i + 1), lista[i])
