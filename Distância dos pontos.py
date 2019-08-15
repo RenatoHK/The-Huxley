701a9171fea3ac85763090d8dc8575fb0613f16d
RenatoHK <48373263+RenatoHK@users.noreply.github.com>
2019-08-15 18:34:32 -0300

    Delete Distância dos pontos.py

--- Distância dos pontos.py
+++
@@ -1,7 +0,0 @@
-x1 = float(input('Insira X1'))
-y1 = float(input('Insira Y1'))
-x2 = float(input('Insira X2'))
-y2 = float(input('Insira Y2'))
-
-res = (((x2 - (x1))**2) + ((y2 - (y1))**2)) ** 0.5
-print('%.2f' % res)
